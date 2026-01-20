7d8c473d8a5213a10e2b9d8a1da30d7764d4a9b0
agzet <agzetjik@gmail.com>
2026-01-21 00:15:36 +0600

    jfhvx

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY =  os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["16.171.40.54", "127.0.0.1", "localhost"]
 
 
 # Application definition
