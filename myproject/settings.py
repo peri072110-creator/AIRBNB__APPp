99b0d60d382734edf2c171fee56baa9cb0a5c7ec
agzet <agzetjik@gmail.com>
2026-01-27 18:51:32 +0600

    kokota

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,6 +140,7 @@ MODELTRANSLATION_LANGUAGES = ('ru', 'en')
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
